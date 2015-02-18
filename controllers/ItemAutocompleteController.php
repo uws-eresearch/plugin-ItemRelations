27ed90a0d6c0316edde3f3db8d2a82c5d81bfd1c
Thom Mc <thomas.mcintyre@uts.edu.au>
2015-02-18 05:55:58 +0000

    add filter to api item autocomplete - element id can be passed and filter returned results to people

--- controllers/ItemAutocompleteController.php
+++ controllers/ItemAutocompleteController.php
@@ -28,32 +28,81 @@ class ItemRelations_ItemAutocompleteController extends Omeka_Controller_Abstract
      */
     public function getAction()
     {
-   
         $request = $this->getRequest();
 //        $recordType = $request->getParam('api_record_type');
         $resource = $request->getParam('api_resource');
         $apiParams = $request->getParam('api_params');
         
+        $term = $apiParams[0];
+        $dcfieldid = empty($apiParams[1]) ? null : $apiParams[1];
+        
         $db = $this->_helper->db->getTable("element_texts");
 /*
 SELECT DISTINCT et1.record_id, et1.text FROM omeka_element_texts et1 INNER JOIN omeka_element_texts et2 ON et1.record_id = et2.record_id WHERE et1.element_id=50 and et1.record_type="Item" and et2.record_type="Item" AND (et2.element_id = 50 or et2.element_id = 52) AND et2.text LIKE '%mullet%';
 */
-        $full_table_name = $db->getTableName('element_texts');
+/*
+        $bootstrap = Zend_Registry::get('bootstrap');
+        $currentUser = $bootstrap->getResource('CurrentUser');
+        $acl = $bootstrap->getResource('Acl');
+*/
+        
+       
+        $fullname_element_texts = $db->getTableName('element_texts');
+        $fullname_elements = $db->getTablePrefix(). 'elements';
+        $fullname_item_types_elements = $db->getTablePrefix(). 'item_types_elements';
+        $fullname_item_types = $db->getTablePrefix(). 'item_types';
 
      //   $select = $db->getSelect();
 
-        $sql = "
-            SELECT DISTINCT et1.record_id, et1.text
-            FROM            {$full_table_name} et1
-            INNER JOIN      {$full_table_name} et2
-                ON et1.record_id = et2.record_id
-            WHERE           et1.element_id=50
-                AND         et1.record_type='Item'
-                AND         et2.record_type='Item'
-                AND         (et2.element_id = 50 or et2.element_id = 52)
-                AND         et2.text LIKE ?";
+        // if DC field is a person, limit results to people...
+        if (in_array($dcfieldid, array(22, 24, 35)))    // contributor, creator, publisher
+        {
+            $sql = "
+                SELECT DISTINCT et1.record_id, et1.text
+                FROM            {$fullname_element_texts} et1
+                INNER JOIN      {$fullname_element_texts} et2
+                    ON et1.record_id = et2.record_id
+                        INNER JOIN  omeka_items i
+                            ON et2.record_id = i.id
+                                    INNER JOIN	omeka_item_types it
+                                        on i.item_type_id = it.id
+                WHERE           et1.element_id=50
+                    AND         et1.record_type='Item'
+                    AND         et2.record_type='Item'
+                    AND         (et2.element_id = 50 or et2.element_id = 52)
+                    AND         et2.text LIKE ?
+                    AND         it.id = 12";
+   /*         
+                SELECT DISTINCT et1.record_id, et1.text
+                FROM            {$fullname_element_texts} et1
+                INNER JOIN      {$fullname_element_texts} et2
+                    ON et1.record_id = et2.record_id
+                        INNER JOIN  {$fullname_item_types_elements} ite
+                            on et2.element_id = ite.element_id
+                WHERE           et1.element_id=50
+                    AND         et1.record_type='Item'
+                    AND         et2.record_type='Item'
+                    AND         (et2.element_id = 50 or et2.element_id = 52)
+                    AND         et2.text LIKE ?
+                    AND         ite.item_type_id = 12";
+//die($sql); */
+            $data = $db->getTable('Element')->fetchObjects($sql, array('%'. $term . '%'));
+        }
+        else
+        {
+            $sql = "
+                SELECT DISTINCT et1.record_id, et1.text
+                FROM            {$fullname_element_texts} et1
+                INNER JOIN      {$fullname_element_texts} et2
+                    ON et1.record_id = et2.record_id
+                WHERE           et1.element_id=50
+                    AND         et1.record_type='Item'
+                    AND         et2.record_type='Item'
+                    AND         (et2.element_id = 50 or et2.element_id = 52)
+                    AND         et2.text LIKE ?";
 
-        $data = $db->getTable('Element')->fetchObjects($sql, array('%'. $apiParams[0]. '%'));
+            $data = $db->getTable('Element')->fetchObjects($sql, array('%'. $term . '%'));
+        }        
 
         $output = array();
         foreach ($data as $datum)
@@ -71,6 +120,5 @@ SELECT DISTINCT et1.record_id, et1.text FROM omeka_element_texts et1 INNER JOIN 
 
 //        print_r($data);
 //        $this->_helper->jsonApi($data);
-
     }
 }
