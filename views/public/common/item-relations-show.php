03d513a7e82497e45c281d949806f842895c0e1d
Thom Mc <thomas.mcintyre@uts.edu.au>
2015-05-27 01:18:32 +0000

    Make multiple item relations of the same type expandable and hideable

--- views/public/common/item-relations-show.php
+++ views/public/common/item-relations-show.php
@@ -6,12 +6,33 @@
     <table>
         <?php foreach ($subjectRelations as $term => $subjectRelation): ?>
         <tr>
-            <td><?php echo __('This Item'); ?></td>
+            <td valign='top'><?php echo __('This Item'); ?></td>
             <td valign='top'><span title="<?php echo html_escape($subjectRelation['relation_description']); ?>"><?php echo $term; ?></span></td>
             <td>
-            <?php foreach ($subjectRelation as $relation): ?>
+            <?php $i = 0; foreach ($subjectRelation as $relation): ?>
                 Item: <a href="<?php echo url('items/show/' . $relation['object_item_id']); ?>"><?php echo $relation['object_item_title']; ?></a><br>
+            <?php
+                ++$i;
+                if ($i == 2 && count($subjectRelation) > 2)
+                {
+                    echo "<div id='vis-{$term}' style='display: none;'>";
+                }
+            ?>
             <?php endforeach; ?>
+            <?php
+                if (count($subjectRelation) > 2)
+                {
+                    echo <<<EOB
+                    </div>
+                    <div id='showmore-{$term}'>
+                        <a href='javascript:;' onclick='document.getElementById("vis-{$term}").style.display="block"; document.getElementById("showless-{$term}").style.display="block"; document.getElementById("showmore-{$term}").style.display="none";'>See more...</a>
+                    </div>
+                    <div id='showless-{$term}' style='display: none;'>
+                        <a href='javascript:;' onclick='document.getElementById("vis-{$term}").style.display="none"; document.getElementById("showmore-{$term}").style.display="block"; document.getElementById("showless-{$term}").style.display="none";'>See less...</a>
+                    </div>
+EOB;
+                }
+            ?>
             </td>
         </tr>
         <?php endforeach; ?>
